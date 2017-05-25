a4fa248bb5b8505b37f7c4a49a751caf27f81737
tybaek <btydntjddls@gmail.com>
2017-05-25 23:04:10 +0900

    ui 변화 표현 및 스레드코드 추가

--- sk_app/app/src/main/java/com/example/mac/sk_app/Loading.java
+++ sk_app/app/src/main/java/com/example/mac/sk_app/Loading.java
@@ -1,28 +1,181 @@
 package com.example.mac.sk_app;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-//로딩 화면
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+import static android.R.attr.value;
+
 public class Loading extends AppCompatActivity {
 
+    String androidID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
         LoadingActivity();
+
     }
     private void LoadingActivity() {
         Handler handler = new Handler() {
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
-                startActivity(new Intent(Loading.this, ChoiceRole.class));  //3초 동안 화면에 나타난 후에 역할 선택 액티비티로 이동
-                finish();
+                androidID=android.provider.Settings.Secure.getString(getBaseContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+
+                MyFireBaseInstanceIDService createToken= new MyFireBaseInstanceIDService();
+                createToken.onTokenRefresh();
+                String token=createToken.getToken();
+                String param = "getMethod=checkJoin&androidID="+androidID+"&token="+token;
+                AppClient appClient = new AppClient();
+
+                appClient.execute(param);
+                // finish();
             }
         };
-        handler.sendEmptyMessageDelayed(0, 3000);       //3초 동안 화면에 나타냄
+        handler.sendEmptyMessageDelayed(0, 3000);
+    }
+    public class AppClient extends AsyncTask<String, Void, String> {
+
+        private String url="http://222.108.243.141:8089/sk_tomcat/receiveData.do";
+        private String result;
+        private HttpURLConnection conn;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+
+        /* "http://silverkeeper.iptime.org/sk_server/receiveSilverData";*/
+
+        }
+
+        @Override
+        public String doInBackground(String... params) {
+            try {
+                URL obj = new URL(url);
+                conn = (HttpURLConnection) obj.openConnection();
+
+                /*conn.setReadTimeout(10000);*/
+            /*conn.setConnectTimeout(15000);*/
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setUseCaches(false);
+                conn.setDefaultUseCaches(false);
+                conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded;charset=utf-8");
+
+
+                OutputStream out = conn.getOutputStream();
+                String test ="";
+                for(int i=0;i<params.length;i++)
+                    test = test + params[i];
+                Log.v("///",test);
+                /*System.out.println()*/
+                out.write(test.getBytes());
+                out.flush();
+                out.close();
+                conn.connect();
+
+                InputStream in = conn.getInputStream();
+             /*   BufferedReader rd=new BufferedReader(new InputStreamReader(in));
+                String line;
+                while((line=rd.readLine())!=null) {
+
+                }*/
+                ByteArrayOutputStream bout = new ByteArrayOutputStream();
+
+                byte[] buf = new byte[1024 * 8];
+                int length = 0;
+                while ((length = in.read(buf)) != -1) {
+                    bout.write(buf, 0, length);
+                }
+
+                System.out.println(new String(bout.toByteArray(), "UTF-8"));
+                Log.w("server",new String(bout.toByteArray(), "UTF-8"));
+
+                result=new String(bout.toByteArray(), "UTF-8");
+                System.out.println("asyncTask result:"+result+"\n");
+                Log.v("asyncTaskResult",result);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+            HashMap<String,String> results=new HashMap<String,String>();
+            StringTokenizer st=new StringTokenizer(result,"&");
+            while(st.hasMoreTokens())
+            {
+                String data = st.nextToken();
+                String temp=data;
+                StringTokenizer st2=new StringTokenizer(temp,"=");
+                while(st2.hasMoreTokens())
+                {
+                    results.put(st2.nextToken(),st2.nextToken());
+                }
+            }
+            System.out.println("value:"+value);
+            Intent intent;
+            String res=results.get("result");
+            if(res.contains("new"))
+           {
+                intent=new Intent(Loading.this, ChoiceRole.class);
+               System.out.println("res:"+res+" value="+results.get("androidID"));
+               intent.putExtra("androidID",androidID);
+               startActivity(intent);
+            }
+            else if(res.contains("silver"))
+            {
+                intent=new Intent(Loading.this, ViewData.class);
+                System.out.println("res:"+res+" value="+results.get("silverID"));
+                intent.putExtra("silverID",results.get("silverID"));
+                startActivity(intent);
+            }
+            else if(res.contains("keeper")) {
+                intent=new Intent(Loading.this, ViewKdata.class);
+                System.out.println("res:"+res+" value="+results.get("keeperID"));
+                intent.putExtra("keeperID",results.get("keeperID"));
+                startActivity(intent);
+            }
+            finish();
+            return result;
+
+        }
+
+        @Override
+        protected void onPostExecute(String res)
+        {
+            super.onPostExecute(res);
+            System.out.println("Result!:"+res);
+
+
+           /* Enumeration<String> parameterNames = request.getParameterNames();
+            while (parameterNames.hasMoreElements())
+            {
+                String name = (String) parameterNames.nextElement();
+                if(name.equals("getMethod"))
+                    type = request.getParameter(name);
+                else
+                    dataMap.put(name, request.getParameter(name));
+                System.out.println(name + "=" + request.getParameter(name));
+            }
+            return dataMap;*/
+            conn.disconnect();
+
+
+        }
+
+
     }
 }
--- sk_app/app/src/main/java/com/example/mac/sk_app/ViewData.java
+++ sk_app/app/src/main/java/com/example/mac/sk_app/ViewData.java
@@ -1,19 +1,66 @@
 package com.example.mac.sk_app;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.StringTokenizer;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 /**
  * Created by 차민광01027370165 on 2017-05-11.
  */
 
 //보호자가 생체정보를 보는 화면
 public class ViewData extends AppCompatActivity {
+    TextView heartText,walkText,walkCount,heartRate,testIdNum;
+    ImageView statusView;
+    String silverID;
+    HashMap<String,String> results;
+    RequestSilver rs;
+    SendSilverData ssd;
+    RequestSilverStatus rss;
+    String param1,param2,param3;
+    String identifyNumber;
+    Random rand=new Random();
+    int wCount,hRate,conM;
+    boolean connMiBand;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_data);
+        Intent intent=getIntent();
+        silverID=intent.getStringExtra("silverID");
+        System.out.println("SILVERID:"+silverID);
+        param1="getMethod=sendSilverData&silverID="+silverID;
+        rs=new RequestSilver();
+        param2="getMethod=receiveSilverData&silverID="+silverID;
+        hRate=rand.nextInt(101)+50;
+        wCount=rand.nextInt(201);
+        conM=rand.nextInt(2);
+        if(conM==1)
+        {
+            connMiBand=true;
+        }
+        else connMiBand=false;
+        param2+="&heartRate="+hRate+"&walkCount="+wCount+"&currentTime="+(new SimpleDateFormat("yyyy-MM-dd-hh-mm-ss")).format(new Date(System.currentTimeMillis()))+"&connMiBand="+connMiBand;
+        rs.execute(param1);
+        ssd=new SendSilverData();
+        ssd.execute(param2);
+        param3="getMethod=checkEmergency&silverID="+silverID;
+        rss=new RequestSilverStatus();
+       rss.execute(param3);
     }
     public void ok(View view) {
         Intent intent = new Intent(this, CheckIdentity.class);
@@ -27,4 +74,291 @@ public class ViewData extends AppCompatActivity {
 
         finish();
     }
+    public class RequestSilver extends AsyncTask<String, Void, String> {
+
+        private String url="http://222.108.243.141:8089/sk_tomcat/receiveData.do";
+        private String result;
+        private HttpURLConnection conn;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+
+        /* "http://silverkeeper.iptime.org/sk_server/receiveSilverData";*/
+
+        }
+
+        @Override
+        public String doInBackground(String... params) {
+            try {
+                URL obj = new URL(url);
+                conn = (HttpURLConnection) obj.openConnection();
+
+                /*conn.setReadTimeout(10000);*/
+            /*conn.setConnectTimeout(15000);*/
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setUseCaches(false);
+                conn.setDefaultUseCaches(false);
+                conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded;charset=utf-8");
+
+
+                OutputStream out = conn.getOutputStream();
+                String test ="";
+                for(int i=0;i<params.length;i++)
+                    test = test + params[i];
+                Log.v("///",test);
+                /*System.out.println()*/
+                out.write(test.getBytes());
+                out.flush();
+                out.close();
+                conn.connect();
+
+                InputStream in = conn.getInputStream();
+
+                ByteArrayOutputStream bout = new ByteArrayOutputStream();
+
+                byte[] buf = new byte[1024 * 8];
+                int length = 0;
+                while ((length = in.read(buf)) != -1) {
+                    bout.write(buf, 0, length);
+                }
+
+                System.out.println(new String(bout.toByteArray(), "UTF-8"));
+                Log.w("server",new String(bout.toByteArray(), "UTF-8"));
+
+                result=new String(bout.toByteArray(), "UTF-8");
+                System.out.println("asyncTask result:"+result+"\n");
+                Log.v("asyncTaskResult",result);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+            return result;
+        }
+        @Override
+        protected void onPostExecute(String res)
+        {
+            super.onPostExecute(res);
+            System.out.println("Result! RequestSilver:"+res);
+            results=new HashMap<String,String>();
+            StringTokenizer st=new StringTokenizer(res,"&");
+            while(st.hasMoreTokens())
+            {
+                String data = st.nextToken();
+                String temp=data;
+                StringTokenizer st2=new StringTokenizer(temp,"=");
+                while(st2.hasMoreTokens())
+                {
+                    String key=st2.nextToken();
+                    String value=st2.nextToken();
+                    System.out.println("key:"+key);
+                    System.out.println("value:"+value);
+                    results.put(key,value);
+                }
+            }
+            System.out.println("Result!:"+res);
+            heartRate=(TextView)findViewById(R.id.heartRate2);
+            walkCount=(TextView)findViewById(R.id.walkCount2);
+            testIdNum=(TextView)findViewById(R.id.testIdNum);
+            heartRate.setText(results.get("heartRate"));
+            walkCount.setText(results.get("walkCount"));
+            testIdNum.setText(results.get("identifyNumber"));
+           /* Enumeration<String> parameterNames = request.getParameterNames();
+            while (parameterNames.hasMoreElements())
+            {
+                String name = (String) parameterNames.nextElement();
+                if(name.equals("getMethod"))
+                    type = request.getParameter(name);
+                else
+                    dataMap.put(name, request.getParameter(name));
+                System.out.println(name + "=" + request.getParameter(name));
+            }
+            return dataMap;*/
+
+            rs=new RequestSilver();
+            rs.execute(param1);
+           conn.disconnect();
+       }
+   }
+    public class RequestSilverStatus extends AsyncTask<String, Void, String> {
+
+        private String url="http://222.108.243.141:8089/sk_tomcat/receiveData.do";
+        private String result;
+        private HttpURLConnection conn;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+
+        /* "http://silverkeeper.iptime.org/sk_server/receiveSilverData";*/
+
+        }
+
+        @Override
+        public String doInBackground(String... params) {
+            try {
+                URL obj = new URL(url);
+                conn = (HttpURLConnection) obj.openConnection();
+
+                /*conn.setReadTimeout(10000);*/
+            /*conn.setConnectTimeout(15000);*/
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setUseCaches(false);
+                conn.setDefaultUseCaches(false);
+                conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded;charset=utf-8");
+
+
+                OutputStream out = conn.getOutputStream();
+                String test ="";
+                for(int i=0;i<params.length;i++)
+                    test = test + params[i];
+                Log.v("///",test);
+                /*System.out.println()*/
+                out.write(test.getBytes());
+                out.flush();
+                out.close();
+                conn.connect();
+
+                InputStream in = conn.getInputStream();
+
+                ByteArrayOutputStream bout = new ByteArrayOutputStream();
+
+                byte[] buf = new byte[1024 * 8];
+                int length = 0;
+                while ((length = in.read(buf)) != -1) {
+                    bout.write(buf, 0, length);
+                }
+
+                System.out.println(new String(bout.toByteArray(), "UTF-8"));
+                Log.w("server",new String(bout.toByteArray(), "UTF-8"));
+
+                result=new String(bout.toByteArray(), "UTF-8");
+                System.out.println("asyncTask result:"+result+"\n");
+                Log.v("asyncTaskResult",result);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+
+            return result;
+        }
+        @Override
+        protected void onPostExecute(String res)
+        {
+            super.onPostExecute(res);
+            StringTokenizer st=new StringTokenizer(res,"=");
+            statusView=(ImageView)findViewById(R.id.statusImage);
+                String status = st.nextToken();
+                String value = st.nextToken();
+            if(value.contains("emergency"))
+            {
+                statusView.setImageResource(R.drawable.red);
+            }
+            else if(value.contains("warning"))
+            {
+                statusView.setImageResource(R.drawable.yellow);
+            }
+            else if(value.contains("safe"))
+            {
+                statusView.setImageResource(R.drawable.green);
+            }
+            System.out.println("Result status!:"+res);
+            rss=new RequestSilverStatus();
+            rss.execute(param3);
+            conn.disconnect();
+        }
+    }
+    public class SendSilverData extends AsyncTask<String, Void, String> {
+
+        private String url="http://222.108.243.141:8089/sk_tomcat/receiveData.do";
+        private String result;
+        private HttpURLConnection conn;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+
+        /* "http://silverkeeper.iptime.org/sk_server/receiveSilverData";*/
+
+        }
+
+        @Override
+        public String doInBackground(String... params) {
+            try {
+                URL obj = new URL(url);
+                conn = (HttpURLConnection) obj.openConnection();
+
+                /*conn.setReadTimeout(10000);*/
+            /*conn.setConnectTimeout(15000);*/
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setUseCaches(false);
+                conn.setDefaultUseCaches(false);
+                conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded;charset=utf-8");
+
+
+                OutputStream out = conn.getOutputStream();
+                String test ="";
+                for(int i=0;i<params.length;i++)
+                    test = test + params[i];
+                Log.v("///",test);
+                /*System.out.println()*/
+                out.write(test.getBytes());
+                out.flush();
+                out.close();
+                conn.connect();
+
+                InputStream in = conn.getInputStream();
+
+                ByteArrayOutputStream bout = new ByteArrayOutputStream();
+
+                byte[] buf = new byte[1024 * 8];
+                int length = 0;
+                while ((length = in.read(buf)) != -1) {
+                    bout.write(buf, 0, length);
+                }
+
+                System.out.println(new String(bout.toByteArray(), "UTF-8"));
+                Log.w("server",new String(bout.toByteArray(), "UTF-8"));
+
+                result=new String(bout.toByteArray(), "UTF-8");
+                System.out.println("asyncTask result:"+result+"\n");
+                Log.v("asyncTaskResult",result);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+
+            return result;
+        }
+        @Override
+        protected void onPostExecute(String res)
+        {
+            super.onPostExecute(res);
+            System.out.println("Result!:"+res);
+
+            param2="getMethod=receiveSilverData&silverID="+silverID;
+            hRate=rand.nextInt(101)+50;
+            wCount=rand.nextInt(201);
+            conM=rand.nextInt(2);
+            if(conM==1)
+            {
+                connMiBand=true;
+            }
+            else connMiBand=false;
+            param2+="&heartRate="+hRate+"&walkCount="+wCount+"&currentTime="+(new SimpleDateFormat("yyyy-MM-dd-hh-mm-ss")).format(new Date(System.currentTimeMillis()))+"&connMiBand="+connMiBand;
+            ssd=new SendSilverData();
+            ssd.execute(param2);
+            conn.disconnect();
+        }
+    }
+
 }
